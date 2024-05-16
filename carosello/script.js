7a58da3384723bdf2ae8a04784ec6af231b8c8ff
Asja <asja.bbk@gmail.com>
2024-05-16 17:13:59 +0200

    esercizio carosello

--- carosello/script.js
+++ carosello/script.js
@@ -1,6 +1,43 @@
+let immagini = [
+    "img/img1.jpg",
+    "img/img2.jpg",
+    "img/img3.jpg",
+    "img/img4.jpg"
+]
+
+
+let current = 0
 let caruselElement = document.querySelector(".carusel")
 
 let imgElement = document.createElement("img")
-imgElement.src = "img/img1.jpg"
+imgElement.classList.add ("img")
+imgElement.src = immagini[current]
 
-caruselElement.appendChild(imgElement)
+caruselElement.append(imgElement)
+
+let nextElement = document.querySelector(".next")
+
+function next() {    
+    if(current < immagini.length - 1 ){
+        current++
+        console.log(current)
+        imgElement.src = immagini[current]
+    }else{
+        current = 0
+        imgElement.src = immagini[current]
+
+    }
+}
+
+nextElement.addEventListener("click",next)
+
+let prevElement = document.querySelector(".previous")
+function prev() {
+    if(current > 0) {
+        current--
+        console.log(current)
+        imgElement.src = immagini[current]
+    } 
+}
+prevElement.addEventListener("click",prev )
+
