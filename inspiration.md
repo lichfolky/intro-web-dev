de5a2f17df594a8760714006f108d710073c77be
Mattia Folcarelli <mattia.folcarelli@gmail.com>
2024-05-16 16:36:28 +0200

    ins

---
+++ inspiration.md
@@ -0,0 +1,11 @@
+## Ispirazione siti
+
+https://www.awwwards.com/
+https://www.behance.net/
+https://www.pinterest.com/
+https://www.instagram.com/
+
+
+## Grafica
+https://www.deviantart.com/
+https://www.artstation.com/
--- menu/script-obj.js
+++ menu/script-obj.js
@@ -1,18 +1,80 @@
 let menuItems = [
-    'PARKER HOUSE ROLL',
-    'GREEN GODDESS SALAD',
-    'THAI CAESAR SALAD',
-    'HONEY WALNUT PRAWNS',
-    'MAPO TOFU STUFFED CABBAGE',
-    'OCTOPUS BURRATA',
-    'BROCCOLI BEEF RAVIOLI',
-    'ORANGE CURRY BUCATINO',
-    'COCONUT CURRY CHOCHOYOTES',
-    'STICKY RICE STUFFED GAME HEN',
-    'MASA FRIED DORADE (Head On / Tail On)',
-    'PANANG LAMB NECK'
-]
+    {
+        name: "PARKER HOUSE ROLL",
+        price: 8,
+        ingredients: ["Miso Honey", "Furikake Dugga", "Fresno Butter"],
+        vegan: false,
+        glutenFree: false
+    }, {
+        name: "GREEN GODDESS SALAD",
+        price: 15,
+        ingredients: ["Little Gem Lettuce", "Coconut Lime Leaf Vinaigrette", "Tarragon", "Pandan Flakes"],
+        vegan: true,
+        glutenFree: true
+    }, {
+        name: "THAI CAESAR SALAD",
+        price: 16,
+        ingredients: ["Frisee", "Lemongrass", "Puffed Rice Crouton", "Smoked Anchovies", "Parmesan"],
+        vegan: false,
+        glutenFree: true
+    }, {
+        name: "HONEY WALNUT PRAWNS",
+        price: 18,
+        ingredients: ["New Caledonian Blue Prawns", "Celeriac Kewpie", "Candied Walnuts", "Horchata Panna Cotta", "Crispy Rice Salad"],
+        vegan: false,
+        glutenFree: true
+    }, {
+        name: "MAPO TOFU STUFFED CABBAGE",
+        price: 16,
+        ingredients: ["Silken Tofu", "Smoked Shiitakes", "Coconut Rice", "Sichuan", "Calabrian Chili Paste"],
+        vegan: true,
+        glutenFree: true
+    }, {
+        name: "OCTOPUS BURRATA",
+        price: 19,
+        ingredients: ["Squid Ink Fry Bread", "Octopus Al Pastor", "Celery Leaf Chermoula", "Potato Confit", "Tomatillo Mostarda"],
+        vegan: false,
+        glutenFree: false
+    }, {
+        name: "BROCCOLI BEEF RAVIOLI",
+        price: 24,
+        ingredients: ["Short Rib", "Dark Soy", "Brown Butter", "Broccolini", "Parmesano Reggiano"],
+        vegan: false,
+        glutenFree: false
+    }, {
+        name: "ORANGE CURRY BUCATINO",
+        price: 23,
+        ingredients: ["Marigold Noodle", "Persimmon Curry", "Apple Chutney", "Pepita Pistou", "Delicata Halo"],
+        vegan: true,
+        glutenFree: false
+    }, {
+        name: "COCONUT CURRY CHOCHOYOTES",
+        price: 23,
+        ingredients: ["Red Curry Fondue", "Blue Masa Dumplings", "Oyster", "Shimeji", "& King Trumpet Mushrooms", "Fried Dill"],
+        vegan: true,
+        glutenFree: true
+    }, {
+        name: "STICKY RICE STUFFED GAME HEN",
+        price: 24,
+        ingredients: ["Banana Ketchup", "Pickled Papaya", "Chicken Gizzard Dirty Rice", "Endive"],
+        vegan: false,
+        glutenFree: true
+    }, {
+        name: "MASA FRIED DORADE (Head On / Tail On)",
+        price: 32,
+        ingredients: ["Hachiya Gazpacho", "Pink Lady Aguachile", "Mussels Escabeche", "Green Zebra Tomatoes", "Fuyu Persimmon", "Pumpkin Spice Labne"],
+        vegan: false,
+        glutenFree: true
+    }, {
+        name: "PANANG LAMB NECK",
+        price: 29,
+        ingredients: ["Saffron Bao", "Persimmon Amba", "Conserva Raro", "Pomegranate Molasses", "Brusselkraut"],
+        vegan: false,
+        glutenFree: false
+    }]
 
+
+// (V-GE)
 let mainElement = document.querySelector("main")
 mainElement.innerHTML = ""
 
@@ -21,7 +83,7 @@ let numberOfSkulls = 1
 for (let i = 0; i < menuItems.length; i++) {
 
     let menuItemName = document.createElement("div")
-    menuItemName.innerText = menuItems[i]
+    menuItemName.innerText = menuItems[i].name + " " + menuItems[i].price
     menuItemName.classList.add("menu-item")
 
     // se l'elemento è un multiplo di 4
@@ -44,9 +106,9 @@ for (let i = 0; i < menuItems.length; i++) {
         if (direction == "right") {
             container.classList.add("right")
         }
-        for (skulls = 0; skulls < numberOfSkulls; skulls++) {
-            let teschietto = document.createElement("div")
-            teschietto.innerText = '💀'
+        for (skulls = 1; skulls <= numberOfSkulls; skulls++) {
+            let teschietto = document.createElement("img")
+            teschietto.src = "img/skull"+skulls+".png"
             teschietto.classList.add("teschietto")
             container.append(teschietto)
         }
@@ -61,56 +123,3 @@ for (let i = 0; i < menuItems.length; i++) {
 
     mainElement.append(menuItemName)
 }
-
-
-// Esempio di ricerca locale
-let searchItem = 'OCTOPUS BURRA TA'
-let found = false
-
-for (item of menuItems) {
-    if (searchItem == item) {
-        // trovato
-        found = true
-    }
-}
-if (found) {
-    console.log("trovato")
-} else {
-    console.log("non trovato")
-}
-
-/*
-for (item of menuItems) {
-    let menuItemName = document.createElement("div")
-    menuItemName.innerText = item
-    menuItemName.classList.add("menu-item","right")
-    mainElement.append(menuItemName)  
-}
-*/
-
-// usare gli oggetti per rappresentare l'informazione
-let itemObj = [
-    {
-        name: "PARKER HOUSE ROLL",
-        price: 8,
-        ingredients: ["Miso Honey", "Furikake Dugga", "Fresno Butter"],
-        vegan: true,
-        glutenFree: false
-    }, {
-        name: "PARKER HOUSE ROLL",
-        price: 8,
-        ingredients: ["Miso Honey", "Furikake Dugga", "Fresno Butter"],
-        vegan: false,
-        glutenFree: false
-    }]
-
-    let menuItemName = document.createElement("div")
-    let itemDescr = itemObj[0].name
-    if(itemObj[0].vegan){
-        itemDescr += " (VEG) "
-    }
-    itemDescr += " " + itemObj[0].price
-
-    menuItemName.innerText = itemDescr
-    menuItemName.classList.add("menu-item","right")
-    mainElement.append(menuItemName)  
